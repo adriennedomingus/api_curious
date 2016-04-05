80a9e9afbffc023cec75c335726fd73258abb6b8
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-05 17:23:19 -0600

    Add Repo PORO
    Inherits from OpenStruct
    Service is called within PORO
    Call to 'all' on PORO done in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     if current_user
       @service = GithubService.new(current_user)
+      @repos = Repository.all(current_user)
     end
   end
 end
