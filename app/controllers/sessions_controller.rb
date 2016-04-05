387f44e6cf1d0a21647d0027b544e40c1efe4f32
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-04-04 18:46:23 -0600

    Style dashboard
    Information is hard coded
    Add css
    Clear auth session on logout as well as session of user id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,14 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by(uid: auth["uid"]) || User.create_with_omniauth(auth)
+    session[:auth] = auth
     session[:user_id] = user.id
     redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
+    session[:auth] = nil
     redirect_to root_path
   end
 end
